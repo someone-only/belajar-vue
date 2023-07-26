e28e4d9967920591adcc62f420eede988c61877c
someone-only <akumemailanda@gmail.com>
2023-07-26 10:26:26 +0700

    menambahkan materi tentang conditional rendering

---
+++ src/components/essentials/conditional-rendering/conditional-if.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const awesome = ref(true);
+</script>
+
+<template>
+  <!-- kita bisa menggunakan if, else if dan else menggunakan v-if v-else-if v-else -->
+  <h1 v-if="awesome">kamu keren!</h1>
+  <h1 v-else></h1>
+  <!-- kita juga bisa menggunakan if statmen di dalam template -->
+</template>
---
+++ src/components/essentials/conditional-rendering/conditional-show.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const ok = ref(true);
+</script>
+
+<template>
+  <!-- conditional menggunakan v-show -->
+  <!-- jika menggunakan v-show elementet akan tetap terender tapi memiliki style display: none; -->
+  <h1 v-show="ok">Ini tampil jika true dan tidak tampil jika false</h1>
+</template>
