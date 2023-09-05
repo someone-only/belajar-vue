dd3c90dec078233571a3c414453a2b11701022b0
someone-only <akumemailanda@gmail.com>
2023-09-05 11:35:13 +0700

    menambahkan materi tentang global dan local registration

---
+++ src/components/component-in-depth/registrations/global-and-local-registration/Global.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// deklarasi component global berada di main.ts di folder src
+// dan component global bisa langsing digunakan tanpa diimport
+</script>
+<template>
+  <h1>Ini Global</h1>
+  <myComponent />
+</template>
